b7a5cfebff51ac21f125a545f56b7c9cf4ade2c8
Lejuene Delantar <107376979+Lingwinee@users.noreply.github.com>
2026-03-05 15:13:01 +0800

    Update Activity

--- Course_Notes/Year_2/Second_Semester/Introduction_to_Intelligent_Systems/Midterm/Guests.py
+++ Course_Notes/Year_2/Second_Semester/Introduction_to_Intelligent_Systems/Midterm/Guests.py
@@ -1,3 +1,12 @@
+"""
+    Goal: Practice list manipulation and user input.
+    Write a script that starts with an empty list called guests. Use a while loop
+    to continuously ask the user for a name to add to the party list.
+        ● If the user types "done", stop the loop and print the final list alphabetically.
+        ● If they type a name already in the list, tell them "They're already on the list!" and don't add it again.
+        ● Bonus: If the list reaches 5 names, print "The party is full!" and exit.
+"""
+
 guests = []
 name = None
 count = 0
